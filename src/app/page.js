d9676c70b481d97a94f3a061952f98392d52ebbc
Nishit Lugun <lugun.nishit@gmail.com>
2024-08-23 00:53:43 +0530

    fifth commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,22 @@
 import React from "react";
-import Main from "./Components/Main";
+import Navbar from "./Components/Navbar";
+import Landing from "./Components/Landing";
+import Work from "./Components/Work";
+import PlayReel from "./Components/PlayReel";
+import Images from "./Components/Images";
+import Spread from "./Components/Spread";
 
 const page = () => {
-  return <Main />;
+  return (
+    <div className="w-full ">
+      <Navbar />
+      <Landing />
+      <Work />
+      <PlayReel />
+      <Images />
+      <Spread />
+    </div>
+  );
 };
 
 export default page;
