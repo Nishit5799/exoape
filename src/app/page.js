a754dd753e954f0702df04f7edd39878dff17eed
Nishit Lugun <lugun.nishit@gmail.com>
2024-08-23 01:12:32 +0530

    sixth commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,39 @@
-import React from "react";
+"use client";
+import React, { useEffect, useRef } from "react";
 import Navbar from "./Components/Navbar";
 import Landing from "./Components/Landing";
 import Work from "./Components/Work";
 import PlayReel from "./Components/PlayReel";
 import Images from "./Components/Images";
 import Spread from "./Components/Spread";
+import LocomotiveScroll from "locomotive-scroll";
 
-const page = () => {
+const Page = () => {
+  const scrollRef = useRef(null);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const locomotiveScroll = new LocomotiveScroll({
+        el: scrollRef.current,
+        smooth: true,
+      });
+
+      return () => {
+        if (locomotiveScroll) locomotiveScroll.destroy();
+      };
+    }
+  }, []);
+
   return (
-    <div className="w-full ">
+    <div
+      className="w-full"
+      ref={scrollRef}
+      style={{
+        overflow: "hidden",
+        position: "relative",
+      }}
+      data-scroll-container
+    >
       <Navbar />
       <Landing />
       <Work />
@@ -19,4 +44,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default Page;
